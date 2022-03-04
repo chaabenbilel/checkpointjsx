6ac30ed30b7da2ec8b0c924bbd282913fa07778a
Your Name <youremail@yourdomain.com>
2022-03-04 11:56:53 +0100

    this is first test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Fromm from './componet/Fromm';
+function App() {
+  return (
+    <div className="App">
+    <Fromm/>
+    </div>
+  );
+}
+
+export default App;
